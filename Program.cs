e2a9ba31a784ee22644147473505817f7e16cc98
Izhakhtml <Izhaklijalem28@gmail.com>
2021-11-17 16:20:56 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,340 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace FirstConsoleApp
+{
+      class Program
+      {
+        static void Main(string[] args)
+        {
+            //int num = 7;
+            //double num2 = 3.4; 
+            //float num3 = 3.4;
+            //string text = "hi there";
+            //char myChar = "h";
+            //bool myBoll = true;
+            //Console.WriteLine("Enter your age:");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num);
+            //if (num > 18)
+            //{Console.WriteLine("good");}
+            //else{Console.WriteLine("young");}
+            int[] numArray = new int[] { 1, 2, 3, 4, 5 };
+            string[] stringArray = new string[4] { "izhak", "baruch", "shani", "matan" };
+            //Console.WriteLine(stringArray[0]);
+            //Console.WriteLine(stringArray[stringArray.Length-1]);
+            //for(int i = 0; i < stringArray.Length; i++)
+            //{
+            //    Console.WriteLine(stringArray[i]);
+            //}
+            //int counter = 0;
+            //while (counter < stringArray.Length)
+            //{
+            //    Console.WriteLine(stringArray[counter]);
+            //    counter++;
+            //}
+
+            //exe 1
+            //Console.WriteLine("hello world");
+
+            //exe 2
+            //Console.WriteLine("izhak");
+
+            //exe 3
+            //Console.WriteLine("enter your name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine(name);
+
+            //exe 4
+            //Console.WriteLine(67);
+
+            //exe 5
+            //Console.WriteLine("enter number");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num);
+
+            //exe 6
+            //Console.WriteLine("Enter number");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num > 25)
+            //{
+            //    Console.WriteLine("HI");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("bye");
+            //}
+
+            //exe 7
+            //Console.WriteLine("enter number");
+            //int num = int.Parse(Console.ReadLine());
+            //int counter = 0;
+            //while (counter < 5) { 
+            //    Console.WriteLine(num);
+            //    counter++;
+            //  }
+
+            //exe 8
+            //Console.WriteLine("enter number");
+            //int num = int.Parse(Console.ReadLine());
+            //int counter = 0;
+            //while (counter < num)
+            //{
+            //    Console.WriteLine(num);
+            //    counter++;
+            //}
+
+            //exe 9
+            //Console.WriteLine("enter name");
+            //string nameInpt = Console.ReadLine();
+            ////Console.WriteLine(nameInpt);
+            //Console.WriteLine("enter sge");
+            //int ageInput = int.Parse(Console.ReadLine());
+            ////Console.WriteLine(ageInput);
+            //int counter = 0;
+            //while (counter < ageInput)
+            //{
+            //    Console.WriteLine(nameInpt);
+            //    counter++;
+            //}
+
+            //exe 10
+            //int num1 = 12;
+            //int num2 = 18;
+            //int num3 = 20;
+            //int num4 = 30;
+            //int[] arrayNum = new int[]{ num1,num2,num3,num4};
+            //int sum = 0;
+            //int avg = 0;
+            //int counter = 0;
+            //while (counter <arrayNum.Length)
+            //{
+            //    sum += arrayNum[counter];
+            //    avg = sum / 4;
+            //    counter++;
+            //}
+            //Console.WriteLine(sum);
+            //Console.WriteLine(avg);
+
+            //exe 11
+            //int width = 40;
+            //int height = 80;
+            //Console.WriteLine(width*height);
+
+            ////exe 12
+            //int num1 = 1;
+            //int num2 = 2;
+            //int num = 0;
+            //Console.WriteLine(num1+" "+num2);
+            //if (num1 < num2) {
+            //    num = num1; 
+            //    num1 = num2; 
+            //    num2 = num; 
+            //}
+            //Console.WriteLine(num1+" "+num2);
+
+            //exe 13
+            //int num1 = int.Parse(Console.ReadLine());
+            //int num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num1+" "+num2);
+
+            //exe 15
+            //string a1 = "a";
+            //string a2 ="1";
+            //Console.WriteLine(a1 + " " + a2);
+            //string str = "";
+            //str = a1;
+            //a1 = a2;
+            //a2 = str;
+            //Console.WriteLine(a1+" "+a2);
+
+            //exe 16
+            //bool a = true;
+            //Console.WriteLine(a);
+
+            //exe 17
+            //int counter = 0;
+            //int sum = 1;
+            //while (counter < 3)
+            //{
+            //    sum = sum * int.Parse(Console.ReadLine());
+            //    counter++;
+            //}
+            //Console.WriteLine(sum);
+
+            //exe 18
+            //float first = float.Parse(Console.ReadLine());
+            //float second = float.Parse(Console.ReadLine());
+            //float result = first / second;
+            //Console.WriteLine(result);
+
+            //exe 19
+            //int num = int.Parse(Console.ReadLine());
+            //num++;
+            //Console.WriteLine(num);
+
+            //exe 20
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num * 3);
+
+            //exe 21
+            //int space = int.Parse(Console.ReadLine());
+            //Console.WriteLine(space*4);
+            //Console.WriteLine(space*(space/2));
+
+            //exe 22
+            //int counter = 0;
+            //while (counter < 100)
+            //{
+            //    Console.WriteLine(counter);
+            //    counter++;
+            //}
+
+            //exe 23
+            //int counter = 0;
+            //while (counter < 50)
+            //{
+            //    if (counter % 2 == 0)
+            //    {
+            //        Console.WriteLine(counter);
+            //    }
+            //    counter++;
+            //}
+            // exe 24
+            //int counter = 0;
+            //while (counter < 50)
+            //{
+            //    if (counter % 3 == 1)
+            //    {
+            //        Console.WriteLine(counter);
+            //    }
+            //    counter++;
+            //}
+
+            // exe 25
+            //int num1 = int.Parse(Console.ReadLine());
+            //int num2 = int.Parse(Console.ReadLine());
+            //int num3 = int.Parse(Console.ReadLine());
+            //int num4 = int.Parse(Console.ReadLine());
+            //int num5 = int.Parse(Console.ReadLine());
+            //int[] arrayMum =new int[] {num1,num2,num3,num4,num5};
+            //int counter = 0;
+            //int sum = 0;
+            //int avg = 0;
+            //while (counter < arrayMum.Length)
+            //{
+            //    sum += arrayMum[counter];
+            //    avg = sum / 5;
+            //    counter++;
+            //}
+            //    Console.WriteLine(sum);
+            //    Console.WriteLine(avg);
+
+            //exe 26
+            //int num1 = int.Parse(Console.ReadLine());
+            //int num2 = int.Parse(Console.ReadLine());
+            //int num3 = int.Parse(Console.ReadLine());
+            //int num4 = int.Parse(Console.ReadLine());
+            //int num5 = int.Parse(Console.ReadLine());
+            //int[] arrayNum = new int[] { num1, num2, num3, num4, num5 };
+            //int min = arrayNum[0];
+            //int max = arrayNum[0];
+            //for (int i = 0; i < arrayNum.Length; i++)
+            //{
+            //    if (arrayNum[i] < min)
+            //    {
+            //        min = arrayNum[i];
+            //    }
+            //    if (arrayNum[i] > max)
+            //    {
+            //        max = arrayNum[i];
+            //    }
+            //}
+            //Console.WriteLine(min);
+            //Console.WriteLine(max);
+
+            //exe 26
+            //int i = int.MinValue;
+            //int counter = 0;
+            //while (counter < 5)
+            //{
+            //    int result = int.Parse(Console.ReadLine());
+            //    i = Math.Max(i,result);
+            //    counter++;
+            //}
+            // Console.WriteLine(i);
+
+            //exe 27
+            //int userNumbers = int.Parse(Console.ReadLine());
+            //int max = int.MinValue;
+            //int min = int.MaxValue;
+            //while (0<userNumbers)
+            //{
+            //max = Math.Max(max, userNumbers);
+            //min = Math.Min(min, userNumbers);
+            //userNumbers = int.Parse(Console.ReadLine());   
+            //}
+            //Console.WriteLine(max);
+            //Console.WriteLine(min);
+            //exe 28
+            //int numberInput = int.Parse(Console.ReadLine());
+            //int i = 0;
+            //while (i < numberInput)
+            //{
+            //    int num = numberInput % 10;
+            //    Console.WriteLine(num);
+            //    int prasNum = (numberInput / 10) - (num / 10);
+            //    numberInput = prasNum;
+            //}
+
+            //exe 29
+            //Console.WriteLine("ENTER NUMBER 0-99");
+            //Random math = new Random();
+            //int number = 45;
+            //int randomNum = math.Next(1, 100);
+            //int counter = 0;
+            //int num = int.Parse(Console.ReadLine());
+            //while (num!=number)
+            //{
+            //    if (num > number)
+            //    {
+            //        Console.WriteLine("you up"+" "+counter);
+            //        num = int.Parse(Console.ReadLine());
+            //    }
+            //    if (num < number)
+            //    {
+            //        Console.WriteLine("you down"+" "+counter);
+            //        num = int.Parse(Console.ReadLine());
+            //    }
+            //    if (num == number)
+            //    {
+            //        Console.WriteLine(counter);
+            //    }
+            //    counter++;
+            //}
+
+            //exe 30
+            //int width = int.Parse(Console.ReadLine());
+            //int heigth = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < heigth; i++)
+            //{
+            //    for(int j = 0; j < width; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //     Console.WriteLine();
+            //}
+
+            //exe 31
+            //int stars = int.Parse(Console.ReadLine());
+            //string p = "";
+            //for(int col = 0; col < stars; col++)
+            //{
+            //    p += "*";
+            //    Console.WriteLine(p) ;
+            //}
+            
+        }
+
+    }
+}         
